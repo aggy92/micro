492810d3ce1f2f3e987fc1e4a4350d2adb258eaf
Kamil Augustyn <augus_k@best.com.pl>
2019-09-19 12:51:41 +0200

    eureka

--- currency-conversion-service/src/main/java/com/aggy/micro/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/aggy/micro/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,10 +2,12 @@ package com.aggy.micro.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.aggy.micro.currencyconversionservice")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
